c2412577815219d337eb20641972d778bd1929bd
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-05-03 10:32:59 +0200

    added socket

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-    console.log("Client was connected", socket.id);
+    console.log("Client was connected:", socket.id);
+    socket.broadcast.emit('user-connected', socket.id); // skickas till alla utom den som precis connectade
+    io.emit('user-connected', socket.id); // till alla som är connected
+    socket.emit('new-user-message', "Welcome!"); // till endast den som precis connectade
 });
 
 server.listen(3000, () => {
